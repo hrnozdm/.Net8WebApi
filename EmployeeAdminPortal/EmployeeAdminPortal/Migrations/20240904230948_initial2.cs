f6e4c606ae1e38ec4b76e3bf4b43ec6e2de37c3c
Harun Özdemir <80276787+hrnozdm@users.noreply.github.com>
2024-09-05 02:51:13 +0300

    Update

---
+++ EmployeeAdminPortal/EmployeeAdminPortal/Migrations/20240904230948_initial2.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeAdminPortal.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DepartmentId",
+                table: "Employees",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "DepartmentId1",
+                table: "Employees",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "Department",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Department", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_DepartmentId1",
+                table: "Employees",
+                column: "DepartmentId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Department_DepartmentId1",
+                table: "Employees",
+                column: "DepartmentId1",
+                principalTable: "Department",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Department_DepartmentId1",
+                table: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Department");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_DepartmentId1",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentId",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentId1",
+                table: "Employees");
+        }
+    }
+}
